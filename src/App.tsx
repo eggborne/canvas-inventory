c3083b126ad2b602bf16b62ef63d6ec949a183f2
eggborne <mike@mikedonovan.dev>
2024-12-09 16:31:22 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+      <header>Loren's canvases</header>
+      <main>Main area</main>
+      <footer>made with ❤️ by mike@mikedonovan.dev</footer>
+    </>
+  )
+}
+
+export default App
